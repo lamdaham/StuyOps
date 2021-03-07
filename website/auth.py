9e0839f1f5ab228c37a0abb610dfc82696198501
lamdaham <ilam21@stuy.edu>
2021-03-07 12:31:30 -0500

    Databases and removed extra directory

---
+++ website/auth.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, render_template, request, flash
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+	data = request.form
+	print(data)
+	return render_template("login.html")
+
+@auth.route('/logout')
+def logout():
+	return "<p>logout</p>"
+
+@auth.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+	if request.method == 'POST':
+		email = request.form.get('email')
+		firstName = request.form.get('firstName')
+		password1 = request.form.get('password1')
+		password2 = request.form.get('password2')
+
+		if len(email) < 4:
+			flash('email is too short', category='error')
+		elif len(firstName)<2:
+			flash('firstName is too short', category='error')
+		elif password1 != password2:
+			flash('password aint match', category='error')
+		elif len(password1) < 7:
+			flash('password too short', category='error')
+		else:
+			flash('success', category='success')
+
+	return render_template("sign_up.html")
---
+++ website/models.py
@@ -0,0 +1,26 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class Ops(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	data = db.Column(db.String(100000000))
+	date = db.Column(db.DateTime(timezone = True))
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class User(db.Model, UserMixin):
+
+	id = db.Column(db.Integer, primary_key=True)
+	email = db.Column(db.String(150), unique=True)
+	password = db.Column(db.String(150))
+	first_name = db.Column(db.String(150))
+	notes = db.relationship('Ops')
+
+
+	# """docstring for User"""
+	# def __init__(self, arg):
+	# 	super(User, self).__init__()
+	# 	self.arg = arg
+	# 	
