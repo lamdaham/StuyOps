9f9a08401c999284f7a04b304eefc20f388fe04b
lamdaham <ilam21@stuy.edu>
2021-03-14 22:00:21 -0400

    Created signup/login/logout

--- website/auth.py
+++ website/auth.py
@@ -1,34 +1,60 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
+	if request.method == 'POST':
+		email = request.form.get('email')
+		password = request.form.get('password')
+		user = User.query.filter_by(email=email).first()
+		if user:
+			if check_password_hash(user.password, password):
+				flash('logged in successfully', category = 'success')
+				login_user(user, remember=True)
+				return redirect(url_for('views.home'))
+			else:
+				flash('incorrect password', category = 'error')
+		else:
+			flash('email does not exist', category = 'error')
 	data = request.form
 	print(data)
-	return render_template("login.html")
+	return render_template("login.html", user = current_user)
 
 @auth.route('/logout')
+@login_required
 def logout():
-	return "<p>logout</p>"
+	logout_user()
+	return redirect(url_for('auth.login'))
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
 	if request.method == 'POST':
 		email = request.form.get('email')
-		firstName = request.form.get('firstName')
+		first_name = request.form.get('firstName')
 		password1 = request.form.get('password1')
 		password2 = request.form.get('password2')
-
-		if len(email) < 4:
-			flash('email is too short')
-		elif len(firstName)<2:
+		user = User.query.filter_by(email=email).first()
+		if user:
+			flash('Email already exist', category='error')
+		elif len(email) < 4:
+			flash('email is too short', category='error')
+		elif len(first_name)<2:
 			flash('firstName is too short', category='error')
 		elif password1 != password2:
 			flash('password aint match', category='error')
 		elif len(password1) < 7:
 			flash('password too short', category='error')
 		else:
+			new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method='sha256'))
+			db.session.add(new_user)
+			db.session.commit()
 			flash('success', category='success')
+			login_user(user, remember=True)
+			return redirect(url_for('views.home'))
 
-	return render_template("sign_up.html")
+	return render_template("sign_up.html", user = current_user)
--- website/views.py
+++ website/views.py
@@ -1,7 +1,13 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-	return render_template("home.html")
+	return render_template("home.html", user = current_user)
+
+@views.route('/ops')
+@login_required
+def ops():
+	return render_template("oppertunities.html", user=current_user)
